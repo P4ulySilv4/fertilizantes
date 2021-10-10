62a131991ea027e7aa3d9101ba7f7953d8916a9a
Pauly Silva <68145475+P4ulySilv4@users.noreply.github.com>
2021-10-10 01:46:32 -0300

    Delete fertilizante.py

--- fertilizante.py
+++
@@ -1,9 +0,0 @@
-#FERTILIZANTE
-hectarea=float(input("ingrese cantidad de hectarea: "))
-comp=float(input("ingrese cantidad de compuesto en %: "))
-matorral=(input("hay matorral? si/no: "))
-compuesto=hectarea*comp
-if compuesto >=10 and matorral== "no":
-	print("es factible la utilizacion de fertilizantes")
-else:
-	print("no es factible la utilizacion de fertilizantes")
